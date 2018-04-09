c81a34f8c67667ac89ae304d8237ca28378adc6c
Raymond <mr_me_9@hotmail.com>
2018-04-09 09:29:03 +0200

    DemoStart

--- src/main/java/com/github/machosmurf/restdemo/administratie/Student.java
+++ src/main/java/com/github/machosmurf/restdemo/administratie/Student.java
@@ -1,17 +1,26 @@
 package com.github.machosmurf.restdemo.administratie;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
 public class Student {
 
-    static int nextStudentNummer = 1;
+    static int nextStudentNummer = 0;
     private int studentNummer;
     private String voornaam;
     private String achternaam;
+    private String wachtwoord;
 
     public Student(String voornaam, String achternaam){
         this.voornaam = voornaam;
         this.achternaam = achternaam;
-        this.studentNummer = nextStudentNummer;
+        this.studentNummer = getNextStudentNummer();
+        this.wachtwoord = "welkom123";
+    }
+
+    public static int getNextStudentNummer(){
         nextStudentNummer++;
+        return nextStudentNummer;
     }
 
     public int getStudentNummer() {
@@ -22,8 +31,11 @@ public class Student {
         return voornaam;
     }
 
-
     public String getAchternaam(){
         return achternaam;
+    }
+
+    public String getWachtwoord(){
+        return wachtwoord;
     }
 }
